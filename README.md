ca3265ff995028a1bc29a8dab7bcb4d407ebd8ae
shenZ <shenAwesome@gmail.com>
2015-07-24 13:31:11 +1000

    init

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # jThread
 A poor man's Thread for JavaScript
+
+The goal of jThread is to mimic the Thread behaviour in JavaScript to solve the 'Callback Hell' problem. In the Web page development, there are mainly 2 types of async logic: Ajax call or UI logic needs user interaction. 
+With callbacks, the code is often tightly coupled, the callback chain can be different to understand or debug. jThread utilizes the 'Thread' or 'Workflow' concept to solve the problem.
+
+when coding with jThread, There are two important Type: Thread and Activity. A Thread mimics the behaviour of a thread in languages like Java or C#, where you can code in a blocking style without callbacks. 
+Activities should only be used inside a thread. They are wrappers around the asynchronous functions. You can create new Activities easily to reuse them later, or just wrap any async logic as anonymous Activity in the Thread.
+
+Imaging this requirement : 
+
+1) ajax call to a server 
+2) display return json on the page
+3) let user select some items
+
+These activities needs to be run following certain orders. 
+
+below is the implementation in jThread.
+
+'''javascript
+var $ = window.jThread; 
+//With 2 functions as parameter, a Thread is  started.
+$(function () {
+	//built in functions(Activities),safe to use inside Thread
+	$.log('start');
+	$.sleep(1000);
+	var repos = $.getJson('https://api.github.com/users/shenAwesome/repos');
+	$.log(repos);
+	//with 1 function as parameter, an Activity is executed.
+	//When interacting with the 'outside world', wrap logic as an activity
+	$(function () {
+		var html = repos.map(function (repo) {
+			return '<label><input type="checkbox" />' + repo.name + '</label></br>';
+		}).join(' ');
+		document.querySelector('#div1').innerHTML = html;
+	});
+	//when the button clicked, this activity finishes
+	$(function (finish) {
+		var btn = document.createElement('button');
+		btn.innerHTML = "Click ME";
+		document.querySelector('#div1').appendChild(btn);
+		btn.onclick = finish;
+	});
+	//now the Thread is finished as the function is done.
+	return "all done";
+}, function (ret) {//called when the thread finishes
+	alert(ret);
+});
+'''
---
+++ src/jThread.js
@@ -0,0 +1,219 @@
+﻿/*-------------------mimic thread--------------------------------------------------------------------------------------------------*/
+(function () {
+    var currentWorker;
+    /** mimic sync behavior*/
+    function run(func, onFinish) {
+        var currenFuncHandle = null;
+        function next() {
+            try {
+                currentWorker = worker;
+                var ret = func.call();
+                //check if the flow needs to be repeated 
+                if (onFinish(ret)) {
+                    worker.step = 0;
+                    worker.act.length = 0;
+                    next();
+                }
+                worker.onStep(-1);
+            } catch (e) {
+                if (e === parseInt(e, 10)) {//go to the step if it's a number
+                    worker.act.length = e;
+                    worker.step = 0;
+                    //if (worker.log) console.log('goto ' + e)
+                    next();
+                } else {//an activity is runing, waiting for it
+                    if (!e.isHandle) throw e;
+                    currenFuncHandle = e;
+                    worker.step = 0;
+                }
+            }
+        }
+        var worker = {
+            onStep: function (step) {
+                //console.log(step);
+            },
+            step: 0,
+            act: [],
+            next: next,
+            ctx: {},
+            cancel: function () {//stop the worker and inform the current running function
+                currenFuncHandle && currenFuncHandle();
+                worker.next = function () { };
+                worker.onStep(-1);
+                onFinish();
+            }
+        };
+        setTimeout(next, 1);
+        return worker;
+    }
+    /** convert async function to sync style, to be used in run*/
+    function sync(func) {
+        return function () {
+            var c = currentWorker;
+            var idx = c.step;
+            //to stop this activity from outside, it is also called when activity finishes
+            var closeHandle;
+            function finish(value) {
+                if (value === undefined) value = null;
+                c.act[idx] = value;
+                setTimeout(function () {
+                    closeHandle();
+                    c.next();
+                }, 1);
+            }
+            finish['ctx'] = c.ctx;
+            if (c.act[idx] === undefined) {
+                c.onStep(c.step);
+                var args = Array.prototype.slice.call(arguments);
+                args.push(finish);
+                closeHandle = func.apply(c.ctx, args) || function () { };
+                //throw the current function handle.
+                closeHandle.isHandle = true;
+                throw closeHandle;
+            } else {
+                c.step++;
+                return c.act[idx];
+            }
+        }
+    }
+
+    //short cuts for run() and sync(func)();
+    function main() {
+        var args = Array.prototype.slice.call(arguments);
+        if (args.length == 2) {
+            return run(args[0], args[1]);
+        } else {
+            var func = args[0];
+            if (func.length == 0) {//no arguments, then it is just a normal sync block
+                func = function (callback) {
+                    var ret = args[0]();
+                    callback(ret);
+                }
+            }
+            sync(func)();
+        }
+    }
+
+    //convert a normal function to sync function, if name is given, it is added to the global 'main'
+    function install(func, name, autoFinish) {
+        var func_;
+        if (autoFinish) {
+            func_ = sync(function () {
+                var args = Array.prototype.slice.call(arguments);
+                var callback = args.pop();
+                func.apply(null, args);
+                callback();
+            });
+        } else {
+            func_ = sync(func);
+        }
+
+        if (name) main[name] = func_;
+        return func;
+    }
+
+    main.sync = install;
+
+    window.jThread = main;
+}());
+
+/**------------------batteries*/
+
+(function () {
+    var $ = window.jThread;
+
+    $.sync(function (msg) {
+        alert(msg);
+    }, 'alert', true);
+
+    $.sync(function (msg) {
+        console.log(msg);
+    }, 'log', true);
+
+    $.sync(function (ele, html) {
+        getEle(ele).innerHTML = html;
+    }, 'html', true);
+
+    $.sync(function (time, callback) {
+        setTimeout(function () {
+            callback();
+        }, time);
+    }, 'sleep');
+
+    function getEle(ele) {
+        if (typeof ele == 'string') {
+            ele = document.querySelector(ele);
+        }
+        return ele;
+    }
+    //ele,key,value,each or ele,obj,each
+    function pairOrObj(args, each) {
+        var ele = getEle(arg[0]);
+        var obj;
+        if (args.length == 3) {//read key value from an object, igrnoe the callback.
+            obj = args[1];
+        } else {
+            obj = {};
+            obj[args[1]] = args[2];
+        }
+        Object.keys(obj).forEach(function (k) {
+            each(ele, k, obj[k]);
+        });
+    }
+
+    $.sync(function (ele, key, value) {
+        pairOrObj(arguments, function (ele, k, v) {
+            ele.style[k] = obj[k];
+        });
+        callback();
+    }, 'css', true);
+
+    $.sync(function (ele, key, value) {
+        pairOrObj(arguments, function (ele, k, v) {
+            ele.setAttribute(k, v);
+        });
+    }, 'attr', true);
+
+
+    function ajax(url, callback) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4) {
+                function parse(str) {
+                    try { str = JSON.parse(str); } catch (e) { }
+                    return str
+                }
+                callback(xmlhttp.status != 404 ? parse(xmlhttp.responseText) : null);
+            }
+        }
+        xmlhttp.open("GET", url, true);
+        setTimeout(function () {
+            xmlhttp.send();
+        }, 1);
+    }
+
+    function getJson(server, data, callback) {
+        if (arguments.length == 2) {
+            callback = data; data = {};
+        }
+        var jsonpCallback = data.callback;
+        var params = [],
+            k;
+        for (k in data) {
+            params.push(k + '=' + encodeURI(data[k]));
+        }
+        var url = server + '?' + params.join('&');
+        if (jsonpCallback) {
+            var script = document.createElement('script');
+            script.src = url;
+            window[jsonpCallback] = callback;
+            document.body.appendChild(script);
+        } else {
+            ajax(url, callback);
+        }
+    }
+
+    $.sync(ajax, 'ajax');
+    $.sync(getJson, 'getJson');
+
+}());
